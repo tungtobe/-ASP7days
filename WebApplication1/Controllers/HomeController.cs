de9e3a5a10ca4ef862ab6652a6fc763302d8c861
tungtobe <tung.tobe@gmail.com>
2018-05-12 10:03:25 +0900

    Lab4 - Using ViewBag

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -10,6 +11,13 @@ namespace WebApplication1.Controllers
     {
         public ActionResult Index()
         {
+            Employee emp = new Employee
+            {
+                FirstName = "Tung",
+                LastName = "Tobe",
+                Salary = 10000
+            };
+            ViewBag.Employee = emp;
             return View();
         }
 
